dfa5d0cf56665ae735ad6d9b353ee931640df03f
Madeleine Ethridge <madeleine.ethridge@gmail.com>
2023-11-23 11:53:44 -0500

    cleanup (#2)
    * cleanup
    
    * formatting error on the readme

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Open_Sans } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const open_sans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={inter.className + " container"}>{children}</body>
+      <body className={open_sans.className + " container"}>{children}</body>
     </html>
   )
 }
