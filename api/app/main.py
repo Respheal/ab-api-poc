021438806b4d809b709608ce0e8a66045a602835
Respheal <madeleine.ethridge@gmail.com>
2023-11-24 15:31:54 -0500

    basic compose services for stack

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from celery import Celery
 
 app = FastAPI()
+
+
+celery = Celery(
+    __name__, broker="redis://redis:6379/0", backend="redis://redis:6379/0"
+)
 
 
 @app.get("/")
@@ -11,3 +17,11 @@ def read_root() -> dict[str, str]:
 @app.get("/test")
 def read_test() -> dict[str, str]:
     return {"msg": "Hello World :3"}
+
+
+@celery.task
+def divide(x, y):
+    import time
+
+    time.sleep(5)
+    return x / y
