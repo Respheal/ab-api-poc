19a597e5837b3a6be1dcdaeaa4d685851cbd634f
Madeleine Ethridge <madeleine.ethridge@gmail.com>
2023-11-25 19:36:16 -0500

    docker-compose (#4)
    * basic compose services for stack
    
    * multistage builds
    
    * setting up a start script
    
    * throwing in the proof of concept file
    
    * fixing tox and mypy errors

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from celery import Celery
 
 app = FastAPI()
+
+
+celery = Celery(
+    __name__, broker="redis://redis:6379/0", backend="redis://redis:6379/0"
+)
 
 
 @app.get("/")
@@ -11,3 +17,8 @@ def read_root() -> dict[str, str]:
 @app.get("/test")
 def read_test() -> dict[str, str]:
     return {"msg": "Hello World :3"}
+
+
+@celery.task
+def divide(x: int, y: int) -> float:
+    return float(x / y)
